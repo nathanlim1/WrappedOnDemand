ec2f7807eb63e44e6b17398e02401a2387c330fc
Logan Costello <logancostello2022@gmail.com>
2024-10-24 16:40:51 -0700

    removed unused imports

--- packages/react-frontend/src/App.jsx
+++ packages/react-frontend/src/App.jsx
@@ -1,5 +1,3 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import React, {useState, useEffect} from 'react'
 import SpotifyWebApi from "spotify-web-api-js";
