104bf6d18d7d9f4a816adf4e1fd1497f0c208179
Logan Costello <logancostello2022@gmail.com>
2024-10-23 22:01:01 -0700

    spotify log in and simple api calls

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -33,11 +33,6 @@
  app.use(express.static(__dirname + '/public'))
     .use(cors())
     .use(cookieParser());
- 
-
-app.get('/', function(req, res) {
-    res.send('Welcome! Go to <a href="/login">Login with Spotify</a> to start.');
-});
 
  app.get('/login', function(req, res) {
  
@@ -45,7 +40,7 @@ app.get('/', function(req, res) {
    res.cookie(stateKey, state);
  
    // your application requests authorization
-   var scope = 'user-read-private user-read-email';
+   var scope = 'user-read-private user-read-email user-library-read user-top-read user-library-modify';
    res.redirect('https://accounts.spotify.com/authorize?' +
      querystring.stringify({
        response_type: 'code',
@@ -104,13 +99,13 @@ app.get('/', function(req, res) {
          });
  
          // we can also pass the token to the browser to make requests from there
-         res.redirect('/#' +
+         res.redirect('http://localhost:5173/#' +
            querystring.stringify({
              access_token: access_token,
              refresh_token: refresh_token
            }));
        } else {
-         res.redirect('/#' +
+         res.redirect('http://localhost:5173/#' +
            querystring.stringify({
              error: 'invalid_token'
            }));
--- packages/react-frontend/src/App.jsx
+++ packages/react-frontend/src/App.jsx
@@ -1,14 +1,107 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import React, {useState, useEffect} from 'react'
+import SpotifyWebApi from "spotify-web-api-js";
+
+const spotifyApi = new SpotifyWebApi();
+
+const getTokenFromUrl = () => {
+  return window.location.hash.substring(1).split('&').reduce((initial, item) => {
+    let parts = item.split('=');
+    initial[parts[0]] = decodeURIComponent(parts[1]);
+    return initial
+  }, {});
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [spotifyToken, setSpotifyToken] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [topArtists, setTopArtists] = useState([]);
+  const [topTracks, setTopTracks] = useState([]);
+
+
+  useEffect(() => {
+    console.log("This is what we got from the url: ", getTokenFromUrl());
+    const spotifyToken = getTokenFromUrl().access_token;
+    window.location.hash = "";
+    console.log("this is our spotify token: ", spotifyToken);
+
+    if (spotifyToken) {
+      setSpotifyToken(spotifyToken);
+
+      // use spotify api
+      spotifyApi.setAccessToken(spotifyToken);
+      console.log("Current Access Token:", spotifyApi.getAccessToken());
+
+      spotifyApi.getMe()
+      .then((user) => {
+        console.log(user);
+      })
+      .catch((err) => {
+        console.log(err)
+      });
+
+      setLoggedIn(true);
+    }
+  }, [])
+
+  const getMyTopArtists = () => {
+    spotifyApi.getMyTopArtists()
+        .then((response) => {
+            console.log(response);
+            const artistNames = response.items.map(artist => artist.name);
+            setTopArtists(artistNames);
+        })
+        .catch((err) => {
+            console.error('Error fetching top artists:', err);
+        });
+  };
+
+  const getMyTopTracks = () => {
+    spotifyApi.getMyTopTracks()
+        .then((response) => {
+            console.log(response);
+            const trackNames = response.items.map(track => track.name);
+            setTopTracks(trackNames);
+        })
+        .catch((err) => {
+            console.error('Error fetching top artists:', err);
+        });
+  };
+
 
   return (
     <>
-      Hello world
+      {!loggedIn && <a href="http://localhost:8000/login">Login to Spotify</a>}
+      {loggedIn && (
+        <>
+          <div>Yay you logged in!</div>
+          <div>Let's try an API call to get some information about you...</div>
+          <button onClick={() => getMyTopArtists()}>Get Top Artists</button>
+          {topArtists.length > 0 && (
+            <div>
+              Your Top Artists:
+              <ol>
+                {topArtists.map((artist, index) => (
+                  <li key={index}>{artist}</li>
+                ))}
+              </ol>
+            </div>
+          )}
+          <button onClick={() => getMyTopTracks()}>Get Top Artists</button>
+          {topTracks.length > 0 && (
+            <div>
+              Your Top Tracks:
+              <ol>
+                {topTracks.map((track, index) => (
+                  <li key={index}>{track}</li>
+                ))}
+              </ol>
+            </div>
+          )}
+        </>
+      )}
     </>
   )
 }
