afafcc97d45e21e765c64e4080d7de448f3efbd0
Logan Costello <logancostello2022@gmail.com>
2024-10-24 16:54:11 -0700

    example react component to track components file

---
+++ packages/react-frontend/src/components/example-component.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// This file is just an example component so the component file is tracked by git
+// Try to make any reusable code as a component and keep them here
+
+const ExampleComponent = () => {
+  return (
+    <div>
+      <h2>Hello from ExampleComponent!</h2>
+      <p>This is a basic example of a React component.</p>
+    </div>
+  );
+};
+
+export default ExampleComponent;
