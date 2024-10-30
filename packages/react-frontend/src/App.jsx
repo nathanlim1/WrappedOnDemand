e2e51e5d3ca965cd0fee8e69df4a175b1e04a0c9
Logan Costello <logancostello2022@gmail.com>
2024-10-29 18:35:30 -0700

    added navbar to app

--- packages/react-frontend/src/App.jsx
+++ packages/react-frontend/src/App.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './pages/login.jsx';
 import Home from './pages/home.jsx'; 
+import Navbar from "./components/navbar/navbar.jsx";
+import Page2 from "./pages/page2.jsx";
+import Page3 from "./pages/page3.jsx";
 
 // This is the very first page "/"
 // It will watch the URL and change whats on the screen based on that
@@ -11,10 +14,13 @@ import Home from './pages/home.jsx';
 const App = () => {
   return (
     <Router>
+      <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/page2" element={<Page2 />}/>
+        <Route path="/page3" element={<Page3 />}/>
       </Routes>
     </Router>
   );
