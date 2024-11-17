a08e41739e8c8ffa112444c202d12a0ef4b86062
Logan Costello <logancostello2022@gmail.com>
2024-11-16 17:14:59 -0800

    track page loads forever for demo

--- packages/react-frontend/src/pages/tracks.jsx
+++ packages/react-frontend/src/pages/tracks.jsx
@@ -1,6 +1,7 @@
 import '../index.css'
 import React, {useState, useEffect} from 'react'
 import { useSpotifyApi } from '../SpotifyContext'; 
+import LoadingSpinner from '../components/loadingSpinner';
 
 function TrackPage({time_range}) {
     const spotifyApi = useSpotifyApi();
@@ -20,21 +21,7 @@ function TrackPage({time_range}) {
   };  
 
   return (
-    <>
-        <h3>Page 3</h3>
-        <p>This is just here for testing the navbar</p>
-        <button onClick={() => getUsersTopArtists()}>Get Top Artists</button>
-        {topArtists.length > 0 && (
-        <div>
-            Your Top Artists:
-            <ol>
-            {topArtists.map((artist, index) => (
-                <li key={index}>{artist}</li>
-            ))}
-            </ol>
-        </div>
-        )}
-    </>
+    <LoadingSpinner/>
   )
 }
 
