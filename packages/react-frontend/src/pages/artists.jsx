d54de39ddcb949239dc95f9cf9a388d492edc641
Logan Costello <logancostello2022@gmail.com>
2024-11-16 16:47:53 -0800

    updated artist page to use artist preview component

--- packages/react-frontend/src/pages/artists.jsx
+++ packages/react-frontend/src/pages/artists.jsx
@@ -1,40 +1,68 @@
 import '../index.css'
 import React, {useState, useEffect} from 'react'
 import { useSpotifyApi } from '../SpotifyContext'; 
+import { getTopNArtists } from '../utils/getTopUtils';
+import ArtistPreview from '../components/artistPreview/artistPreview';
 
 function Page2({time_range}) {
     const spotifyApi = useSpotifyApi();
+    const [artists1month, setArtists1Month] = useState([]);
+    const [artists6month, setArtists6Month] = useState([]);
+    const [artistsLifetime, setArtistsLifetime] = useState([]);
     const [topArtists, setTopArtists] = useState([]);
 
-  // uses spotifyApi to get users top artists
-  const getUsersTopArtists = () => {
-    spotifyApi.getMyTopArtists({time_range: time_range})
-        .then((response) => {
-            console.log(response);
-            const artistNames = response.items.map(artist => artist.name);
-            setTopArtists(artistNames);
-        })
-        .catch((err) => {
-            console.error('Error fetching top artists:', err);
-        });
-  };  
+    // When the page loads, get the top 25 artists for each time range
+    useEffect(() => {
+        const fetchArtists = async () => {
+            try {
+
+                setArtists1Month(await getTopNArtists(spotifyApi, 25, "short_term"));
+                setArtists6Month(await getTopNArtists(spotifyApi, 25, "medium_term"));
+                setArtistsLifetime(await getTopNArtists(spotifyApi, 25, "long_term"));
+            } catch (err) {
+                console.error('Error fetching artists:', err);
+            }
+        };
+
+        fetchArtists();
+        console.log(topArtists[0])
+    }, [spotifyApi]);
+
+    // When timerange changes, update the currently displayed list
+    useEffect(() => {
+        if (time_range === "short_term") {
+            setTopArtists(artists1month);
+        } else if (time_range === "medium_term") {
+            setTopArtists(artists6month);
+        } else if (time_range === "long_term") {
+            setTopArtists(artistsLifetime);
+        }
+    }, [spotifyApi, time_range, artists1month, artists6month, artistsLifetime]);
 
   return (
-    <>
-        <h3>Page 2</h3>
-        <p>This is just here for testing the navbar</p>
-        <button onClick={() => getUsersTopArtists()}>Get Top Artists</button>
-        {topArtists.length > 0 && (
-        <div>
-            Your Top Artists:
-            <ol>
-            {topArtists.map((artist, index) => (
-                <li key={index}>{artist}</li>
-            ))}
-            </ol>
+    <div className='flex flex-col items-center min-h-screen text-white bg-gradient-to-br from-zinc-800 to-zinc-950'>
+        {/* Title */}
+        <p className='text-center font-bold mb-4 mt-4 text-4xl'>Your Top Artists</p>
+        {/* List of Artist Previews */}
+        <div className='space-y-4 mb-4'>
+            {topArtists.map((a, i) => <ArtistPreview artist={a} index={i + 1}/>)}
         </div>
-        )}
-    </>
+        {/* Buttons to see more/all */}
+        <div className="flex space-x-4 mb-20">
+            <button
+                className="bg-zinc-800 text-white font-semibold py-3 px-8 rounded-full shadow-lg transform transition-transform hover:scale-105 active:scale-95 hover:shadow-xl focus:outline-none hover:text-[#00FF7F] w-36"
+                style={{ border: 'none' }}
+            >
+                See More
+            </button>
+            <button
+                className="bg-zinc-800 text-white font-semibold py-3 px-8 rounded-full shadow-lg transform transition-transform hover:scale-105 active:scale-95 hover:shadow-xl focus:outline-none hover:text-[#00FF7F] w-36"
+                style={{ border: 'none' }}
+            >
+                See All
+            </button>
+        </div>
+    </div>
   )
 }
 
