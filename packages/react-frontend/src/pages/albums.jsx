984f837baafbc3f46cf94d9d6db32b1e343fa117
Logan Costello <logancostello2022@gmail.com>
2024-11-18 16:45:12 -0800

    updated albums page to use preloaded album information

--- packages/react-frontend/src/pages/albums.jsx
+++ packages/react-frontend/src/pages/albums.jsx
@@ -1,68 +1,33 @@
 import '../index.css';
 import React, { useState, useEffect } from 'react';
-import { useSpotifyApi } from '../SpotifyContext';
-import { getTopNArtists, getTopNTracks, getTopNAlbums } from '../utils/getTopUtils';
+import LoadingSpinner from "../components/loadingSpinner";
 
-function AlbumPage({ time_range }) {
-  const spotifyApi = useSpotifyApi();
 
-  const [artists, setArtists] = useState([]);
-  const [tracks, setTracks] = useState([]);
-  const [albums, setAlbums] = useState([]);
+function AlbumPage({ time_range, allAlbums }) {
+  const [currentlyDisplayed, setCurrentlyDisplayed] = useState([]);
+  const [maxNumDisplayed, setMaxNumDisplayed] = useState(25);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const topArtists = await getTopNArtists(spotifyApi, 5, time_range);
-        setArtists(topArtists);
-        
-        const topTracks = await getTopNTracks(spotifyApi, 5, time_range);
-        setTracks(topTracks);
-        
-        const topAlbums = await getTopNAlbums(spotifyApi, 5, await getTopNTracks(spotifyApi, 200, time_range));
-        setAlbums(topAlbums);
-      } catch (err) {
-        console.error('Error fetching data:', err);
-      }
-    };
+    if (time_range === "short_term") {
+      setCurrentlyDisplayed(allAlbums["1M"]);
+    } else if (time_range === "medium_term") {
+      setCurrentlyDisplayed(allAlbums["6M"]);
+    } else if (time_range === "long_term") {
+      setCurrentlyDisplayed(allAlbums["LT"]);
+    }
+  }, [time_range, allAlbums]);
 
-    fetchData();
-  }, [spotifyApi, time_range]);
-
-  const artistNames = artists.map((a) => a.name);
-  const trackNames = tracks.map((t) => t.name);
-  const albumNames = albums.map((a) => a.name);
-
+  if (currentlyDisplayed.length === 0) {
+    return <LoadingSpinner />;
+  }
   return (
-    <>
-      <h1>Top {artists.length} Artists</h1>
-      <ul>
-        {artistNames.map((name, index) => (
-          <li key={index}>{name}</li>
-        ))}
-      </ul>
-      
-      <h1>Top {tracks.length} Tracks</h1>
-      <ul>
-        {trackNames.map((name, index) => (
-          <li key={index}>{name}</li>
-        ))}
-      </ul>
+    <div>
+      Top Albums
 
-      <h1>Top {albums.length} Albums</h1>
-      <ul>
-        {albumNames.map((name, index) => (
-          <li key={index}>{name}</li>
-        ))}
-      </ul>
-
-      <h1>Top {artists.length} Genres</h1>
-      <ul>
-      {artists.map((artist, index) => (
-          <li key={index}>{artist.genres[0]}</li>
-        ))}
-      </ul>
-    </>
+      {currentlyDisplayed.slice(0, maxNumDisplayed).map((a, i) => (
+        <div>{i + 1}. {a.name}</div>
+      ))}
+      </div>
   );
 }
 
