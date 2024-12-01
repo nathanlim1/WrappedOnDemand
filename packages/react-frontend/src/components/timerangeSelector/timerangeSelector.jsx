a41cb94642d1f05359a69221a185d7849ee5269d
Logan Costello <logancostello2022@gmail.com>
2024-11-30 20:32:56 -0800

    fixing build error: another import fixed

--- packages/react-frontend/src/components/timerangeSelector/timerangeSelector.jsx
+++ packages/react-frontend/src/components/timerangeSelector/timerangeSelector.jsx
@@ -1,5 +1,5 @@
 import { useRef, useEffect } from 'react';
-import './timeRangeSelector.css';
+import './timerangeSelector.css';
 
 const TimeRangeSelector = ({ currentRange, setRange, loggedIn }) => {
   const indicatorRef = useRef(null);
