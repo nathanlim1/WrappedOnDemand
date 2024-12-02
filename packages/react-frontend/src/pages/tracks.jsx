7ae421db8612133b150c32e46f085b32a92f4be5
nathanlim1 <nlim10@calpoly.edu>
2024-12-02 12:56:45 -0800

    Upon clicking generate playlist, the user is now taken to the top of the page, where a success/error message is shown

--- packages/react-frontend/src/pages/tracks.jsx
+++ packages/react-frontend/src/pages/tracks.jsx
@@ -6,6 +6,31 @@ import TrackPreview from "../components/TrackPreview";
 function TrackPage({ time_range, allTracks, spotifyApi }) {
   const [currentlyDisplayed, setCurrentlyDisplayed] = useState([]);
   const [maxNumDisplayed, setMaxNumDisplayed] = useState(25);
+  const [alertMessage, setAlertMessage] = useState("");
+
+  // Function to handle playlist generation
+  const handleGeneratePlaylist = async () => {
+    const result = await generatePlaylist(
+      currentlyDisplayed.slice(0, maxNumDisplayed),
+      spotifyApi,
+      "Top Tracks"
+    );
+
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+
+    if (result.success) {
+      setAlertMessage(
+        "Playlist created successfully! Look for 'Top Tracks' in your Spotify account."
+      );
+    } else {
+      setAlertMessage(
+        "An error occurred and your playlist could not be generated. Please try again later."
+      );
+    }
+  };
 
   // When timerange changes, update the currently displayed list
   useEffect(() => {
@@ -23,6 +48,13 @@ function TrackPage({ time_range, allTracks, spotifyApi }) {
       <p className="text-center font-bold mb-4 mt-4 text-4xl">
         Your Top Tracks
       </p>
+
+      {/* Alert Message */}
+      {alertMessage && (
+        <div className="bg-gradient-to-br from-[#1ED760] to-green-600 text-white py-2 px-4 rounded-xl mb-4">
+          {alertMessage}
+        </div>
+      )}
 
       <div className="space-y-4 mb-4">
         {currentlyDisplayed.slice(0, maxNumDisplayed).map((t, i) => (
@@ -49,13 +81,7 @@ function TrackPage({ time_range, allTracks, spotifyApi }) {
         <button
           className="bg-zinc-800 text-white font-semibold py-3 px-8 rounded-full shadow-lg transform transition-transform hover:scale-105 active:scale-95 hover:shadow-xl focus:outline-none hover:text-[#00FF7F] w-36"
           style={{ border: "none" }}
-          onClick={() =>
-            generatePlaylist(
-              currentlyDisplayed.slice(0, maxNumDisplayed),
-              spotifyApi,
-              "Top Tracks Playlist"
-            )
-          }
+          onClick={() => handleGeneratePlaylist()}
         >
           Generate Playlist
         </button>
