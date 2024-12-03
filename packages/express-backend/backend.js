bb7f812338eb3efba643c6f7521a6039072fe1e8
nathanlim1 <nlim10@calpoly.edu>
2024-12-02 23:42:51 -0800

    Retrying deployment

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -331,7 +331,7 @@ app.get("/", (req, res) => {
   res.send("Backend is running");
 });
 
-// Start server
+// Start server using default of 8080
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server Configuration Diagnostics:`);
