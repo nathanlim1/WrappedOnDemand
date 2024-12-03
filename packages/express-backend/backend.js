4c6a63007da838037ac310cb47655cda81664169
Logan Costello <logancostello2022@gmail.com>
2024-12-03 12:46:28 -0800

    changed port

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -331,8 +331,8 @@ app.get("/", (req, res) => {
   res.send("Backend is running");
 });
 
-// Start server using default of 8181
-const PORT = process.env.PORT || 8181;
+// Start server using default of 8080
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server Configuration Diagnostics:`);
   console.log(`Listening on PORT: ${PORT}`);
