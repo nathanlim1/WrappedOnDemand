b6a708d0628014c879cd4a6a3ea5e364d1b72871
nathanlim1 <nlim10@calpoly.edu>
2024-12-04 03:35:14 -0800

    changes for deployment

--- packages/express-backend/utils/spotifyUtils.js
+++ packages/express-backend/utils/spotifyUtils.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import fs from "fs";
 import path from "path";
 
-const allGenresPath = path.resolve("../genres.json");
+const allGenresPath = path.resolve(__dirname, "../genres.json");
 const allGenres = JSON.parse(fs.readFileSync(allGenresPath, "utf-8"));
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
