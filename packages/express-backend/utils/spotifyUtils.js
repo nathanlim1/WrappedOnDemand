9c48026e1b7bf13eafb5d70dffb98fecf0f6a589
nathanlim1 <nlim10@calpoly.edu>
2024-12-04 03:42:39 -0800

    changes for deployment

--- packages/express-backend/utils/spotifyUtils.js
+++ packages/express-backend/utils/spotifyUtils.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const allGenresPath = path.resolve(__dirname, "../genres.json");
 const allGenres = JSON.parse(fs.readFileSync(allGenresPath, "utf-8"));
