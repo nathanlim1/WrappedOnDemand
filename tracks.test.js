4e8bf111a9722bd46d1268e7a8aede841f4f3e48
Steven Wong <stevenwong2726@gmail.com>
2024-12-05 23:55:18 -0800

    added 3 button tests

---
+++ tracks.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Tracks from "./packages/react-frontend/src/pages/tracks";
+import { render, fireEvent, cleanup } from "@testing-library/react";
+
+afterEach(cleanup);
+
+test("see more button", () => {
+    const onClick = jest.fn();
+    const { getByText } = render(<Tracks {..."See More"} onClick={onClick} />)
+    fireEvent.click(getByText("See More"));
+    expect(onClick).toHaveBeenCalled();
+});
+
+test("see all button", () => {
+    const onClick = jest.fn();
+    const { getByText } = render(<Tracks {..."See All"} onClick={onClick} />)
+    fireEvent.click(getByText("See All"));
+    expect(onClick).toHaveBeenCalled();
+});
+
+test("create playlist button", () => {
+    const onClick = jest.fn();
+    const { getByText } = render(<Tracks {..."Create Playlist"} onClick={onClick} />)
+    fireEvent.click(getByText("Create Playlist"));
+    expect(onClick).toHaveBeenCalled();
+});
